0225664290ff105cbc7df997edaebdee0ea9a58d
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-13 12:14:49 +0100

    Add explanation of rock, paper, scissors, and planning of the program

---
+++ script.js
@@ -0,0 +1,12 @@
+// Rock, Paper, Scissors
+
+// Rock, Paper, Scissors is a two player game
+// Rock beats scissors, scissors beats paper, and paper beats rock
+// There is a countdown where players count before playing their choice
+
+// Planning:
+
+// The program will run in console, no user interface needed
+// The user will need to enter a choice between rock, paper, scissors
+// The computer will randomly select a choice
+// The game will have 5 rounds, track scores and declare a winner at the end
