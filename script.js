d9625dd2ce8b988d117a90989833fe6e7893a821
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-13 12:26:37 +0100

    Add pseudocode for basic functions of program

--- script.js
+++ script.js
@@ -10,3 +10,15 @@
 // The user will need to enter a choice between rock, paper, scissors
 // The computer will randomly select a choice
 // The game will have 5 rounds, track scores and declare a winner at the end
+
+// Pseudocode:
+
+// Get player choice
+// Generate computer choice
+// If player choice = computer choice, print "tie"
+// If player choice = "rock", and computer choice = "scissors", print "win"
+// If player choice = "paper" and computer choice = "rock", print "win"
+// if player choice = "scissors" and computer choice = "paper", print "win"
+// Else print "lose"
+// Repeat game 5 times
+// Print winner of the game
